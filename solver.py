fccfede2141d1fdc757a2f1bca8e2843dac76ea7
sarafanshul <anshulsaraf3@gmail.com>
2020-02-17 22:47:54 +0530

    asdvkjbsbcv

--- solver.py
+++ solver.py
@@ -1,64 +1,58 @@
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
+'''
+this package helps in processing and checking
+'''
 
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
+def find_empty(grid:list) -> tuple:
+	'''
+	this function finds the empty spaces in the 
+	grid and return as None/tuple
+	'''
+	for i in range(len(grid)):
+		for j in range(len(grid[0])):
+			if grid[i][j] == 0:
+				return (i, j)  # row, col
 
-            if solve(bo):
-                return True
+	return None
 
-            bo[row][col] = 0
+def solve(grid:list) -> bool:
+	find = find_empty(grid)
+	if not find: return True
 
-    return False
+	row, col = find
 
+	for i in range(1,10 ,1):
+		if valid(grid, i, (row, col)):
+			grid[row][col] = i
 
-def valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
+			if solve(grid):
+				return True
 
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
+			grid[row][col] = 0
 
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
-                return False
-
-    return True
+	return False
 
 
-def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+def valid(grid:list, num:int, pos:tuple) -> bool:
+	'''
+	checks if input is valid or not
+	'''
+	# Check row
+	for i in range(len(grid[0])):
+		if grid[pos[0]][i] == num and pos[1] != i:
+			return False
 
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
+	# Check column
+	for i in range(len(grid)):
+		if grid[i][pos[1]] == num and pos[0] != i:
+			return False
 
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
+	# Check grid[]
+	gridx_x = pos[1] // 3
+	gridx_y = pos[0] // 3
 
+	for i in range(gridx_y*3, gridx_y*3 + 3):
+		for j in range(gridx_x * 3, gridx_x*3 + 3):
+			if grid[i][j] == num and (i,j) != pos:
+				return False
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
-
-    return None
+	return True
